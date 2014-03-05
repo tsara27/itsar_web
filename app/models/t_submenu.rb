df4bd6264adeb87b51790f8472425ee6529cd15f
Tsara Fatma L <tsara_tf@bitbucket.org>
2014-03-05 08:26:05 +0700

    Add some testing (minor)

--- app/models/t_submenu.rb
+++ app/models/t_submenu.rb
@@ -2,7 +2,7 @@ class TSubmenu < ActiveRecord::Base
   attr_accessible :t_menu_id, :menu_name, :url, :visible_to, :t_user_id,:id
   validates :menu_name, :presence => true, :format => { :with => /^([a-z0-9 ])+$/i }, :length => { :in => 3..50 }
   validates :url, :presence => true, :format => { :with => /^([a-z0-9\-\/#])+$/i }, :length => { :in => 1..100 }
-  
+  validates :t_menu_id, :presence => true
   belongs_to :t_menu
   belongs_to :t_user
 end
--- app/models/t_user.rb
+++ app/models/t_user.rb
@@ -5,6 +5,10 @@ class TUser < ActiveRecord::Base
   validates :usrnme, :presence => true, :format => { :with => /^([a-z0-9_])+$/i }, :length => { :in => 5..25 }
   validates :passwd, :confirmation => true, :presence => true, :length => { :in => 5..25 }
   validates :mail, :presence => true, :format => { :with => /^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/i }, :length => { :in => 5..25 }
+  validates :gndr, :presence => true
+  validates :t_usertype_id, :presence => true
+  validates :t_itsar_id, :presence => true
+  validates :t_user_id, :presence => true
 
   before_save :enc_passwd 
   after_save :clear_passwd
